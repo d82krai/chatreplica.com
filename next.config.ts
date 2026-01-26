54edd4dd1dac8b38c6daec650ba78be05c4dc314
Deepak Rai <deep.k.rai@gmail.com>
2026-01-26 22:37:57 +0530

    sdf

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactStrictMode: true,
   compress: true,
   poweredByHeader: false,
   images: {
-    formats: ["image/avif", "image/webp"],
+    unoptimized: true,
   },
 };
 
